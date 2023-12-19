fcfcad3360a3ee1e67109ef97772dbd235d62b4b
Harsh Tuwar <tuwarharsh08@gmail.com>
2023-12-19 21:54:18 +0530

    cleanup bad components

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { db } from '@/lib/db';
-
-import Board from './board';
-import { Form } from './form';
 
 const OrganizationIdPage = async () => {
-	const boards = await db.board.findMany();
-
 	return (
 		<div className='flex flex-col space-y-4'>
-			<Form />
-			<div className='space-y-2'>
-				{boards.map(board => (
-					<Board title={board.title} id={board.id} key={board.id} />
-				))}
-			</div>
 		</div>
 	);
 }
