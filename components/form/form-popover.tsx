73a2da52fe3370df1cf2f9a2382b12b35a98e090
Harsh Tuwar <tuwarharsh08@gmail.com>
2023-12-20 09:50:09 +0530

    form popover ui implemented

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -2,15 +2,18 @@
 
 
 import { useAction } from '@/hooks/useActions';
+import { Button } from '@/components/ui/button';
 import { createBoard } from '@/actions/create-board';
 import { 
 	Popover,
+	PopoverClose,
 	PopoverContent,
 	PopoverTrigger
 } from '@/components/ui/popover';
 
 import { FormInput } from './form-input';
 import { FormSubmit } from './form-submit';
+import { X } from 'lucide-react';
 
 
 interface FormPopoverProps {
@@ -40,6 +43,24 @@ const FormPopover = ({
 				<div className='text-sm font-medium text-center text-neutral-600 pb-4'>
 					Create board
 				</div>
+				<PopoverClose asChild>
+					<Button
+						className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600'
+						variant={'ghost'}
+					>
+						<X  className='h-4 w-4 '/>
+					</Button>
+				</PopoverClose>
+				<form className='space-y-4'>
+					<div className='space-y-4'>
+						<FormInput
+							id='title'
+							label='Board title'
+							type='text'
+						/> 
+					</div>
+					<FormSubmit className='w-full'>Create</FormSubmit>
+				</form>
 			</PopoverContent>
 		</Popover>
 	)
