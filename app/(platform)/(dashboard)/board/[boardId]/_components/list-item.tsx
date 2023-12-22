f8ce966a3d3d08a253f29035d33031d7f087858d
Harsh Tuwar <tuwarharsh08@gmail.com>
2023-12-22 13:03:52 +0530

    form textarea component implemented

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import ListHeader from './list-header';
-
 import { ListWithCards } from '@/types/types';
+import { ElementRef, useRef, useState } from 'react';
+
+import CardForm from './card-form';
+import ListHeader from './list-header';
 
 interface ListItemProps {
 	index: number;
@@ -13,10 +15,31 @@ const ListItem = ({
 	index,
 	data
 }: ListItemProps) => {
+	const textareaRef = useRef<ElementRef<'textarea'>>(null);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const disableEditing = () => {
+		setIsEditing(false);
+	};
+
+	const enableEditing = () => {
+		setIsEditing(true);
+		setTimeout(() => {
+			textareaRef.current?.focus();
+		})
+	}
+
 	return (
 		<li className='shrink-0 h-full w-[272px] select-none'>
 			<div className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'>
-				<ListHeader data={data} />
+				<ListHeader data={data} onAddCard={enableEditing} />
+				<CardForm
+					listId={data.id}
+					ref={textareaRef}
+					isEditing={isEditing}
+					enableEditing={enableEditing}
+					disableEditing={disableEditing}
+				/>
 			</div>
 		</li>
 	)
