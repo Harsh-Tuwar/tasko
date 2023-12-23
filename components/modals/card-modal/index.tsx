1144b505231f7b1b74cc7ed15465c0a18a45eba4
Harsh Tuwar <tuwarharsh08@gmail.com>
2023-12-23 15:56:48 +0530

    updating description works

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -7,6 +7,7 @@ import { Dialog, DialogContent } from '@/components/ui/dialog';
 import { fetcher } from '@/lib/fetcher';
 
 import Header from './header';
+import Description from './description';
 
 const CardModal = () => {
 	const id = useCardModal((state) => state.id);
@@ -25,6 +26,13 @@ const CardModal = () => {
 		>
 			<DialogContent>
 				{!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+				<div className='grid grid-cols-1 md:grid-cols-4 md:gap-4'>
+					<div className='col-span-3'>
+						<div className='w-full space-y-6'>
+							{!cardData ? <Description.Skeleton /> : <Description data={cardData} />}
+						</div>
+					</div>
+				</div>
 			</DialogContent>
 		</Dialog>
 	);
