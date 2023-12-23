110ad474d6b4a441d170f9d7f75be906420ad43d
Harsh Tuwar <tuwarharsh08@gmail.com>
2023-12-23 14:57:56 +0530

    basic card info modal initialized

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -2,7 +2,9 @@
 
 import { Card } from '@prisma/client';
 
-import { Draggable, Droppable } from '@hello-pangea/dnd';
+import { Draggable } from '@hello-pangea/dnd';
+
+import { useCardModal } from '@/hooks/use-card-modal';
 
 interface CardItemProps {
 	index: number;
@@ -13,6 +15,8 @@ const CardItem = ({
 	card,
 	index
 }: CardItemProps) => {
+	const cardModal = useCardModal();
+	
 	return (
 		<Draggable draggableId={card.id} index={index}>
 			{(provided) => (
@@ -21,6 +25,7 @@ const CardItem = ({
 					{...provided.draggableProps}
 					ref={provided.innerRef}
 					role='button'
+					onClick={() => cardModal.onOpen(card.id)}
 					className='truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm'
 				>
 					{card.title}
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Toaster } from 'sonner';
 import { ClerkProvider } from '@clerk/nextjs';
 
+import ModalProdiver from '@/components/providers/modal-provider';
+
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<ClerkProvider>
 			<Toaster />
+			<ModalProdiver />
 			{children}
 		</ClerkProvider>
 	);
