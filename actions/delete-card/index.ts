3312ebbf50aef6d2ba234fe6a317ce5cff8389da
Harsh Tuwar <tuwarharsh08@gmail.com>
2023-12-24 21:52:06 +0530

    fixed some bugs

--- actions/delete-card/index.ts
+++ actions/delete-card/index.ts
@@ -10,7 +10,6 @@ import { DeleteCard } from './schema';
 import { InputType, ReturnType } from './types';
 import { createAuditLog } from '@/lib/create-audit-log';
 import { ACTION, ENTITY_TYPE } from '@prisma/client';
-import { decreaseAvailableCount } from '@/lib/org-limit';
 
 const handler = async (data: InputType): Promise<ReturnType> => {
 	const { userId, orgId } = auth();
@@ -35,8 +34,6 @@ const handler = async (data: InputType): Promise<ReturnType> => {
 				}
 			}
 		});
-
-		await decreaseAvailableCount();
 		
 		await createAuditLog({
 			entityId: card.id,
